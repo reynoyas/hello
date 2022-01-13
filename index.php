a626dc01203b4d4d0ad2f2a1a55116f1eaf2cadf
Yasaira Reynoso <reynoso.yasaira@student.greenriver.edu>
2022-01-13 17:42:06 -0500

    Added views

--- index.php
+++ index.php
@@ -11,7 +11,9 @@
 
     // Define a default route
     $f3->route('GET /', function (){
-        echo "<h1>Hello, World!</h1>";
+        //echo "<h1>Hello, World!</h1>";
+        $view = new Template();
+        echo $view->render('views/home.html');
     });
     // Run fat-free
     $f3->run();
